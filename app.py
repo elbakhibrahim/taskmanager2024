be8274d8d376c7f1a1ef640bb60e4a762addc0df
elbakhibrahim <106595858+elbakhibrahim@users.noreply.github.com>
2023-12-27 01:30:55 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+
+import streamlit as st
+
+# Add your app's code here
+
+# Example: Display a simple title and text
+st.title("My Streamlit App")
+st.write("This is my first Streamlit app in Colab!")
